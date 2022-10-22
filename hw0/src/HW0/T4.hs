d3aa156999ee8e43d357793396e649905b7f7a0e
bnkspbrus <bnkspbrus@gmail.com>
2022-10-22 22:54:40 +0300

    modify T4

--- hw0/src/HW0/T4.hs
+++ hw0/src/HW0/T4.hs
@@ -9,10 +9,10 @@ repeat' x = fix (x:)
 map' :: (a -> b) -> [a] -> [b]  -- behaves like Data.List.map
 map' = fix (\rec f arr -> if null arr then [] else f (head arr) : rec f (tail arr))
 fib :: Natural -> Natural       -- computes the n-th Fibonacci number
-fib = fix f
+fib x = fst (fix f x)
   where 
-    f _ 0 = 0
-    f _  1 = 1
-    f rec n = rec (n - 1) + rec (n - 2)
+    f _ 0 = (0, 0)
+    f _  1 = (1, 0)
+    f rec n = let (a, b) = rec (n - 1) in (a + b, a)
 fac :: Natural -> Natural       -- computes the factorial
 fac = fix (\rec n -> if n <= 1 then 1 else n * rec (n-1))
