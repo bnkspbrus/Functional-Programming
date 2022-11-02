023e1c5c10e04f3cc97292dbcba18c01dedc25a4
bnkspbrus <bnkspbrus@gmail.com>
2022-11-02 21:48:54 +0300

    third task

---
+++ hw2/src/HW2/T3.hs
@@ -0,0 +1,22 @@
+module HW2.T3 where
+
+import HW2.T1
+import HW2.T2 (concatList)
+
+joinOption :: Option (Option a) -> Option a
+joinOption None = None
+joinOption (Some a) = a
+
+joinExcept :: Except e (Except e a) -> Except e a
+joinExcept (Error e) = Error e
+joinExcept (Success a) = a
+
+joinAnnotated :: Semigroup e => Annotated e (Annotated e a) -> Annotated e a
+joinAnnotated ((a :# e1) :# e2) = a :# (e2 <> e1)
+
+joinList :: List (List a) -> List a
+joinList Nil = Nil
+joinList (x :. xs) = concatList x (joinList xs)
+
+joinFun :: Fun i (Fun i a) -> Fun i a
+joinFun (F f) = F (\i -> let (F g) = f i in g i)
